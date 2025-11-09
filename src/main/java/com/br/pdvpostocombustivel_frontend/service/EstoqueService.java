3cb89e1a4f8b6694faa19f63668cfaba71f2acb6
Damassss <pescariaurbana@gmail.com>
2025-11-09 18:01:16 -0300

    New

--- src/main/java/com/br/pdvpostocombustivel_frontend/service/EstoqueService.java
+++ src/main/java/com/br/pdvpostocombustivel_frontend/service/EstoqueService.java
@@ -1,6 +1,5 @@
 package com.br.pdvpostocombustivel_frontend.service;
 
-
 import com.br.pdvpostocombustivel_frontend.model.dto.EstoqueRequest;
 import com.br.pdvpostocombustivel_frontend.model.dto.EstoqueResponse;
 import org.springframework.stereotype.Service;
@@ -13,8 +12,11 @@ import java.util.List;
 public class EstoqueService {
 
     private static final String BASE_URL = "http://localhost:8080/api/v1/estoques";
+
+    // INJEÇÃO CORRETA
     private final RestTemplate restTemplate;
 
+    // CONSTRUTOR COM INJEÇÃO AUTOMÁTICA
     public EstoqueService(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
     }
